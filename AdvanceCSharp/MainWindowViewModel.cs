4918bc5480a24fe1267e99e100bd7996e1f21aa8
JagmeetGurm <jagmeetgurm@yahoo.com>
2017-04-01 22:32:19 -0700

    adds age property to student

--- AdvanceCSharp/MainWindowViewModel.cs
+++ AdvanceCSharp/MainWindowViewModel.cs
@@ -3,19 +3,36 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace AdvanceCSharp
 {
     public class MainWindowViewModel: INotifyPropertyChanged
     {
+        public MainWindowViewModel()
+        {
+            Model = new Student();//{FirstName = "Jagmeet", LastName = "Gurm" };
+            Thread.Sleep(4000);
+           // EditStudent(Model);
+        }
+        private void EditStudent(Student student)
+        {
+            
+            student.LastName = "Gill";
+        }
         private Student _model;
         public Student Model
         {
             get { return _model; }
             set { _model = value; NotifyPropertyChanged("Model"); }
         }
+        
+        private void Test()
+        {
 
+        }
         public event PropertyChangedEventHandler PropertyChanged;
         private void NotifyPropertyChanged(String info)
         {
--- AdvanceCSharp/Student.cs
+++ AdvanceCSharp/Student.cs
@@ -9,5 +9,6 @@ namespace AdvanceCSharp
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public int Age { get; set; }
     }
 }
