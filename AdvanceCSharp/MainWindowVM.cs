25b690ca9d31ce6e0807c1cef819f41981c9d8b7
JagmeetGurm <jagmeetgurm@yahoo.com>
2017-04-25 21:34:10 -0700

    adds watermark for a textbox

---
+++ AdvanceCSharp/MainWindowVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceCSharp
+{
+    public class MainWindowVM : INotifyPropertyChanged
+    {
+        private string _phNo;
+
+        public string PhNo
+        {
+            get { return _phNo; }
+            set { string _phNo = value; NotifyPropertyChanged("PhNo"); }
+        }
+        
+
+        private void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
