b851ddfcb9513bba50eb9adb85954f8a30c2e8aa
JagmeetGurm <jagmeetgurm@yahoo.com>
2017-04-25 19:48:54 -0700

    adds expander

---
+++ AdvanceCSharp/MainVM.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace AdvanceCSharp
+{
+    public class MainVM: INotifyPropertyChanged
+    {
+        public MainVM()
+        {
+            Persons = new List<Person>();
+            Persons.Add(new Person { FName = "Jim", LName = "Gurm", Age = 28 });
+            Persons.Add(new Person { FName = "Garry", LName = "Gill", Age = 35 });
+            Persons.Add(new Person { FName = "Jag", LName = "Singh", Age = 25 });
+
+            //
+        }
+
+        private List<Person> _persons;
+        public List<Person> Persons
+        {
+            get { return _persons; }
+            set { _persons = value; NotifyPropertyChanged("Persons"); }
+        }
+
+       
+        private void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
--- AdvanceCSharp/MainWindow.xaml.cs
+++ AdvanceCSharp/MainWindow.xaml.cs
@@ -23,35 +23,9 @@ namespace AdvanceCSharp
         public MainWindow()
         {
             InitializeComponent();
-            //action example: always returns void and is a delegate(function pointer).
-            Action<int> ActionExample = new Action<int>(Print);
-           // ActionExample(5);
+            this.DataContext = new MainVM(); 
 
-            //Func example
-            Func<int, int, int> FuncExample = new Func<int, int, int>(Add);
-            FuncExample(4, 5);
-            ActionExample(result);
-
-            //Predicate Example
-            Predicate<int> PredicateExample = new Predicate<int>(Greater);
-            Result.Content =  PredicateExample(8);
-          
-
-        }
-        private int result;
-        private int Add(int a , int b)
-        {
-            result = a + b;
-            return result;
-        }
-        private void Print(int res)
-        {
-            Result.Content = res;
         }
         
-        private bool Greater(int val1)
-        {
-            return val1 > result;
-        }
     }
 }
